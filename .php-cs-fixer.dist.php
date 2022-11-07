4f8279f091ef755aa1f01d13661552db4b42cf4d
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2022-11-07 07:00:16 +0100

    Fix PHP CS rule `global_namespace_import`
    https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/rules/import/global_namespace_import.rst
    https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/6662

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,6 +5,11 @@ $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 $config = new PhpCsFixer\Config();
 return $config->setRules([
     '@Symfony' => true,
+    'global_namespace_import' => [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
     'yoda_style' => false,
 ])
     ->setFinder($finder);
