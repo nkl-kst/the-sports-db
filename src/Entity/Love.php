fb88d2a652c05a0cb376d9469fb3372270ee743b
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2024-04-08 06:42:40 +0200

    Added missing properties `idVenue` and `strVenue` to `Love` entity

--- src/Entity/Love.php
+++ src/Entity/Love.php
@@ -15,11 +15,13 @@ class Love
     public ?int $idPlayer;
     public ?int $idLeague;
     public ?int $idEvent;
+    public ?int $idVenue;
     public ?string $strSport;
     public ?string $strTeam;
     public ?string $strPlayer;
     public ?string $strLeague;
     public ?string $strEvent;
+    public ?string $strVenue;
     public ?string $strEventPoster;
     public ?string $strEventSquare;
     public ?string $strPlayerThumb;
