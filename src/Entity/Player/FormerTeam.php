234e914ae606c34835c03432c35bbfb141020f29
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2024-06-25 07:43:52 +0200

    Added BC properties for former team entity

--- src/Entity/Player/FormerTeam.php
+++ src/Entity/Player/FormerTeam.php
@@ -2,8 +2,19 @@
 
 namespace NklKst\TheSportsDb\Entity\Player;
 
+use NklKst\TheSportsDb\Entity\BcPropertiesTrait;
+
+/**
+ * @property string $strTeamBadge
+ */
 class FormerTeam
 {
+    use BcPropertiesTrait;
+
+    private const BC_PROPERTIES = [
+        'strTeamBadge' => 'strBadge',
+    ];
+
     public int $id;
     public int $idPlayer;
     public int $idFormerTeam;
@@ -11,7 +22,7 @@ class FormerTeam
     public string $strPlayer;
     public string $strFormerTeam;
     public string $strMoveType;
-    public string $strTeamBadge;
+    public string $strBadge;
     public string $strJoined;
     public string $strDeparted;
 }
---
+++ test/unit/BC/FormerTeamBcTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace NklKst\TheSportsDb\BC;
+
+use NklKst\TheSportsDb\Entity\Player\FormerTeam;
+use NklKst\TheSportsDb\Serializer\Player\FormerTeamSerializer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+class FormerTeamBcTest extends TestCase
+{
+    private FormerTeam $formerTeam;
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp(): void
+    {
+        $formerTeamSerializer = new FormerTeamSerializer(new \JsonMapper());
+        $this->formerTeam     = $formerTeamSerializer->serialize('{ "formerteams": [{ "strBadge": "strTeamBadge" }] }')[0];
+    }
+
+    public function testBcProperties(): void
+    {
+        BcTestUtils::checkBcProperty($this->formerTeam, 'strTeamBadge');
+    }
+}
