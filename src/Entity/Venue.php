f2cf53675ba58e1fc11ac3ade9cea59204fe30b5
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2024-06-28 21:37:15 +0200

    Added all properties for venues entity

--- src/Entity/Venue.php
+++ src/Entity/Venue.php
@@ -4,5 +4,28 @@ namespace NklKst\TheSportsDb\Entity;
 
 class Venue
 {
-    // TODO
+    public int $idVenue;
+    public ?int $idDupe;
+    public string $strVenue;
+    public ?string $strVenueAlternate;
+    public ?string $strSport;
+    public ?string $strDescriptionEN;
+    public ?int $intCapacity;
+    public ?string $strCost;
+    public ?string $strCountry;
+    public ?string $strLocation;
+    public int $intFormedYear;
+    public ?string $strFanart1;
+    public ?string $strFanart2;
+    public ?string $strFanart3;
+    public ?string $strFanart4;
+    public ?string $strThumb;
+    public ?string $strLogo;
+    public ?string $strMap;
+    public ?string $strWebsite;
+    public ?string $strFacebook;
+    public ?string $strInstagram;
+    public ?string $strTwitter;
+    public ?string $strYoutube;
+    public string $strLocked;
 }
