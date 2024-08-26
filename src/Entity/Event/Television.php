37ab515f3a88b3ab405f65e9b2fc6db08e60b7a3
nkl-kst <24235974+nkl-kst@users.noreply.github.com>
2024-08-26 06:27:32 +0200

    Added properties to Television entity
    strEventThumb, strEventPoster, strEventBanner and strEventSquare

--- src/Entity/Event/Television.php
+++ src/Entity/Event/Television.php
@@ -10,6 +10,10 @@ class Television
     public int $idEvent;
     public string $strSport;
     public string $strEvent;
+    public ?string $strEventThumb;
+    public ?string $strEventPoster;
+    public ?string $strEventBanner;
+    public ?string $strEventSquare;
     public int $idChannel;
     public string $strCountry;
     public string $strLogo;
